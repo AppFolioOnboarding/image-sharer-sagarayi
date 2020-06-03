352d4aeb81ed47699febefb84f3d084cc6cece1a
Sagar Ayi <sagar.madhu.ayi@appfolio.com>
2020-06-03 08:42:51 -0700

    SA - Created Footer component and added it to the page

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 class Footer extends Component {
-  /* Implement your Footer component here */
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
+  render() {
+    const title = this.props.title;
+    return (
+      <div>
+        <Row>
+          <Col lg={{ size: 4, offset: 4 }}>
+            <h3 className='text-center' style={{ fontSize: '10px' }}>
+              {title}
+            </h3>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Footer;
