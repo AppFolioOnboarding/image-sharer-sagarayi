583ecc95fc45d7a034213592c45a5f61f8ef2ac3
Sagar Madhu Ayi <sagar.madhu.ayi@appfolio.com>
2020-06-09 12:21:41 -0700

    Refactored feedback form to be present on the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   post 'save_image/create'
   get 'home/filter'
   post 'home/destroy'
-  get 'feedbacks/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :feedbacks, only: [:new]
 
   namespace :api do
     resource :feedbacks, only: [:create]
